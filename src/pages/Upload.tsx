f0f8678bec1d973e7622b768f239953f7dd0144f
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-10-24 22:39:35 +0800

    fix: responsive design at Upload page

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -4,12 +4,12 @@ import FileInput from "@/components/ui/Input/FileInput";
 const Upload = () => {
   return (
     <>
-      <div className="flex justify-center w-full items-center pt-32 sm:pt-20 h-[55dvh] sm:h-[70dvh]">
+      <div className="flex justify-center w-full items-center pt-9 sm:pt-7 ">
         <div className="container px-4 sm:px-0 mx-auto">
-          <h2 className="text-3xl text-center mt-20 sm:mt-14 font-bold py-5">
+          <h2 className="text-3xl text-center font-bold py-5">
             Find out who’s not following you back on Instagram
           </h2>
-          <form className="flex flex-col mx-auto gap-y-7 mt-8 w-max">
+          <form className="flex flex-col mx-auto gap-y-7 mt-6 w-max">
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="followersFile"
@@ -28,12 +28,12 @@ const Upload = () => {
               </label>
               <FileInput id="followersFile" />
             </div>
-            <div className="mt-7">
+            <div className="mt-4">
               <Submit name="checking" />
             </div>
           </form>
           <p className="text-sm text-center my-7">
-            If you don’t have the JSON files yet, visit the&nbsp;
+            If you don't have the JSON files yet, visit the&nbsp;
             <a href="/tutorial" className="underline font-semibold">
               tutorial
             </a>{" "}
